85b964dab5ee4d005adb3240cf438d4df540c6d4
nst <13313020563@163.com>
2017-05-22 17:27:36 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <my-header></my-header>  
     <my-nav></my-nav>
     <router-view></router-view>
     <my-foot></my-foot>
@@ -9,11 +10,13 @@
 <script>
 import myNav from '@/components/nav'
 import myFoot from '@/components/foot'
+import myHeader from '@/components/header'
 export default {
   name: 'app',
   components:{
     myNav,
-    myFoot
+    myFoot,
+    myHeader
   }
 }
 </script>
---
+++ src/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <p> <span>2012</span> 相聚  <span>2016</span> 分别 </p>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    border-top: solid 1px rgba(16, 16, 16, 0.1);
+    box-shadow: 0 0 10px #eee;
+    text-align: center;
+    background-color: rgba(238, 238, 238, 0.6);
+    color: #777;
+  }
+  p {
+    margin-left: 24px;
+    line-height: 30px;
+    font-size: 20px;
+  }
+  span {
+    font-weight: 700;
+    color: rgba(0, 0, 0, 0.9);
+  }
+</style>
